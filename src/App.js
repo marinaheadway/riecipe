7c5f14698d94e5b914e2d50d0a324c215d9a27ee
marinahradway <headway2121@mail.com>
2024-05-14 18:39:17 +0300

    delete console

--- src/App.js
+++ src/App.js
@@ -22,8 +22,7 @@ function App() {
   const getRecipe = async ()=> {
     const response= await fetch (`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${MY_ID}&app_key=${MY_KEY}`);
     const data= await response.json();
-    console.log(data)
-   setMyRecipes(data.hits);
+     setMyRecipes(data.hits);
   }
   getRecipe()
  },[wordSubmitted])
